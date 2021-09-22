52b1ce359b9a2cb73ed231508cb8ee0481400bfa
YeoHoonYun <cjswo9207@gmail.com>
2021-09-22 22:48:05 +0900

    mongo flask test

---
+++ FlaskPyMongo.py
@@ -0,0 +1,16 @@
+# $ pip install Flask-PyMongo
+# python -m pip install "pymongo[srv]"
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://admin:admin@cluster0.8y00i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+
+@app.route("/")
+def home_page():
+    online_users = mongo.db.users.find({"online": True})
+    return render_template("index.html",online_users=online_users)
+
+if __name__ == '__main__':
+    app.run()
