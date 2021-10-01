2c5a72eca7e2e3c32a8bfd110b6f1d2330a837d5
yh.yoon <cjswo9207@naver.com>
2021-10-01 15:07:46 +0900

    commit

--- MongoFind.py
+++ MongoFind.py
@@ -3,16 +3,27 @@ import itertools
 import collections
 import functools
 import operator
+
+import certifi
 import pymongo
 
-client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.8y00i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.8y00i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", tlsCAFile=certifi.where())
 db = client.get_database('MonggoTest')
 collection = db.get_collection('TestData')
 
+contents_type = "twitter"
 from_date = datetime.datetime(2020, 1, 1)
-to_date = datetime.datetime(2020, 12, 31)
+to_date = datetime.datetime(2020, 3, 31)
 
-result = list(collection.find({"type":"twitter","date": {"$gte": from_date, "$lt": to_date}}))
+result = list(collection.find(
+  {
+    # "type":contents_type,
+    "date": {
+      "$gt": from_date,
+      "$lt": to_date
+    }
+  }
+))
 words = [x["words"] for x in result]
 
 counter = functools.reduce(
@@ -23,7 +34,7 @@ counter = functools.reduce(
     for x in words
   )
 )
-print(counter)
+print(counter.most_common(10))
 
 # query = [
 #       {
